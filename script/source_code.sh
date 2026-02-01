108bebe8f8fe771ad1cde037b8665b6832d7082c
koji-oe <ko1114ji@gmail.con>
2026-02-01 13:18:45 +0000

    bashの変数指定修正

--- script/source_code.sh
+++ script/source_code.sh
@@ -6,9 +6,9 @@ find . -type f -name "*.py" | sort | while read -r file; do
   # 先頭の ./ を除いた相対パス
   rel_path="${file#./}"
 
-  echo "\`$rel_path\`"
+  echo "\`${rel_path}\`"
   echo '```python'
-  cat "$file"
+  cat "${file}"
   echo '```'
   echo
 done
